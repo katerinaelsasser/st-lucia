87f3659d23d69ace77c5ee93c765852f738cfba4
Katerina <k.elsasser@aol.co.uk>
2019-06-05 18:19:45 +0000

    adding an additional location of a hotel to the javascript to be marked on the map

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function initMap(){
     // New Map
     var map = new google.maps.Map(document.getElementById('map'), options);
         
+    /*
     // New Marker
     var marker = new google.maps.Marker({
         position:{lat:14.0969,lng:-60.9498},
@@ -20,4 +21,18 @@ function initMap(){
     marker.addListener('click', function(){
         infoWindow.open(map,marker);
     })
+    */
+    // Royalton Resort - Hotel
+    addMarker({lat:14.0969,lng:-60.9498});
+    // St James Club Morgan Bay - Hotel
+    addMarker({lat:14.0406,lng:-60.9706});
+    
+    // Add Marker Function
+    function addMarker(coords){
+        var marker = new google.maps.Marker({
+        position:coords,
+        map:map
+    });
+    
+    }
 }
