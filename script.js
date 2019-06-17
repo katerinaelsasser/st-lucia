3f84ccdee6316d7e3deaf45f193ddb819413370b
Katerina <k.elsasser@aol.co.uk>
2019-06-17 19:12:22 +0000

    adding the locations of the hotels to the index page and creating a modal template that will hold all of the info for each location

--- script.js
+++ script.js
@@ -1,85 +1,8 @@
-function initMap(){
-    // Location of St Lucia
-     var options = {
-        zoom:10,
-        center: {lat: 13.9094,lng:-60.9789}
-        };
+
+        
     // New Map
     var map = new google.maps.Map(document.getElementById('map'), options);
         
-    // Markers
-    // Royalton Resort - Hotel
-    addMarker({
-        coords:{lat:14.0969,lng:-60.9498},
-        content:'<button id="myBtn" class="btn btn-primary">Tester</button>'
-        
-    });
-    // St James Club Morgan Bay - Hotel
-    addMarker({
-        coords:{lat:14.0406,lng:-60.9706},
-        content:'<h6>St James Club Morgan Bay<h6>'
-    });
-    
-    // Jade Mountain Resort - Hotel
-    addMarker({
-        coords:{lat:13.8646,lng:-61.0757},
-        content:'<h6>Jade Mountain Resort<h6>'
-    });
-    
-    // Sandales Regency La Toc - Hotel
-    addMarker({
-        coords:{lat:14.0083,lng:-61.0091},
-        content:'<h6>Sandales Regency La Toc</h6>'
-    });
-    
-    // Sandales Grande St. Lucian - Hotel
-    addMarker({
-        coords:{lat:14.0913,lng:-60.9585},
-        content:'<h6>Sandales Grande St. Lucian</h6>'
-    });
-    
-    // Sandales Halcyon Beach - Hotel
-    addMarker({
-        coords:{lat:14.0308,lng:-60.9772},
-        content:'<h6>Sandales Grande St. Lucian</h6>'
-    });
-    
-    // Mango Beach Inn - Hotel
-    addMarker({
-        coords:{lat:13.9676,lng:-61.0244},
-        content:'<h6>Mango Beach Inn</h6>'
-    });
-    
-    // Hummingbird Resort - Hotel
-    addMarker({
-        coords:{lat:13.8583,lng:-61.0622},
-        content:'<h6>Hummingbird Resort</h6>'
-    });
-    
-    // Bay Gardens Beach Resort - Hotel
-    addMarker({
-        coords:{lat:14.0769,lng: -60.9540},
-        content:'<h6>Bay Gardens Beach Resort</h6>'
-    });
-    
-    // The Harbor Club - Hotel
-    addMarker({
-        coords:{lat:14.0736,lng: -60.9479},
-        content:'<h6>The Harbor Club</h6>'
-    });
-    
-    // Boucan by Hotel Chocolat - Hotel
-    addMarker({
-        coords:{lat:13.8332,lng: -61.0503},
-        content:'<h6>Boucan by Hotel Chocolat</h6>'
-    });
-    
-    // Coco Palm Resort - Hotel
-    addMarker({
-        coords:{lat:14.0697,lng: 60.9554},
-        content:'<h6>Coco Palm Resort</h6>'
-    });
-    
     // Add Marker Function
     function addMarker(props){
         var marker = new google.maps.Marker({
