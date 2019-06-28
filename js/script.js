52b0079b14e994deda9dbadb5872381f9af647b6
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-06-28 15:18:35 +0000

    increasing the zoom

--- js/script.js
+++ js/script.js
@@ -188,6 +188,6 @@ var map;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 13.9094, lng:-60.9789},
-          zoom: 8
+          zoom: 10
         });
       }
