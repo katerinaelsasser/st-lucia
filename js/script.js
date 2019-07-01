9729269c2a5b1050963ca9ee271416a6ae815cc1
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-07-01 17:34:41 +0000

    increasing the zoom by one on the js code

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var map;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 13.9094, lng:-60.9789},
-          zoom: 10
+          zoom: 11
         });
       }
 //end of code for the map
