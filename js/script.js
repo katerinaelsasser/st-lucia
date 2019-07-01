3cddffd17dc34c38aadcacfa9347e63607a83f9b
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-07-01 19:23:07 +0000

    adding another example js code for the markers

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-document.write('<script type="text/javascript" src="hotel.js"></script>');
-//code for the map
+
 var map;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
@@ -8,10 +7,43 @@ var map;
         });
         //end of code for the map
         
+        var locations = [
+        //Hotels
+            //Location of Royalton Hotel
+                {"hotel":"true",title":"Royalton Resort",lat":"14.0969","lng":"-60.9498","content":""},
+            //Location of St James Club Morgan Bay
+                {"hotel":"true","title":"St James Club Morgan Bay","lat":"14.0406","lng":"-60.9706","content":""},
+            //Location of Jade Mountain Resort
+                {"hotel":"true","title":"Jade Mountain Resort","lat":"13.8646","lng":"-61.0757","content":""},
+            //Location of Sandales Regancy La Toc
+                {"hotel":"true","title":"Sandales Regancy La Toc","lat":"14.0083","lng":"-61.0091","content":""},
+            //Location of Sandales Grande St. Lucian
+                {"hotel":"true","title":"Sandales Grande St. Lucian","lat":"14.0913","lng":"-60.9585","content":""},
+            //Location of Sandales Halcyon Beach
+                {"hotel":"true","title":"Sandales Halcyon Beach","lat":"14.0308","lng":"-60.9772","content":""},
+            //Location of Mango Beach Inn
+                {"hotel":"true","title":"Sandales Halcyon Beach","lat":"13.9676","lng":"-61.0244","content":""},
+            //Hummingbird Resort
+                {"hotel":"true","title":"Sandales Halcyon Beach","lat":"13.8583","lng":"-61.0622","content":""},
+            //Bay Gardens Beach Resort
+                {"hotel":"true","title":"Sandales Halcyon Beach","lat":"14.0769","lng":"-60.9540","content":""},
+            //The Harbour Club
+                {"hotel":"true","title":"Sandales Halcyon Beach","lat":"14.0736","lng":"-60.9479","content":""},
+            //Boucan by Hotel Chocolat
+                {"hotel":"true","title":"Sandales Halcyon Beach","lat":"13.8332","lng":"-61.0503","content":""},
+            //Coco Palm Resort
+            {"hotel":"true","title":"Sandales Halcyon Beach","lat":"14.0697","lng":"-60.9554","content":""},
+    
+            ];
       
-      if (hotel === true) {
-          var marker = new google.maps.Marker;
-      } 
+      var marker = locations.map (function(location, i) {
+        return new google.maps.Marker;
+        position: location,
+        label: labels[i % labels.length]
+      });
+      
+      });
+      
       
       
       
