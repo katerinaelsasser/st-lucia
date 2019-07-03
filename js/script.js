7033609c9e727a859c9d9d6ec621bcc1bc20a8c3
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-07-03 16:58:05 +0000

    removing all the css code and replacing it with code for the default map

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 13.9094, lng: -60.9789},
-          zoom: 10
-        });
-      }
+var map;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 13.9094, lng: -60.9789},
+      zoom: 10
+    });
+  }
