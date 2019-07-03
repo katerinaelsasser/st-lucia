e4df28d7b5c250527151546727acb7b1f8eaac47
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-07-03 16:31:48 +0000

    removing the error on the js code that stopped displaying the map

--- js/map.js
+++ js/map.js
@@ -1,8 +1,6 @@
-
-var map;
-      function initMap() {
+function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 13.9094, lng: -60.9789},
-          zoom: 8
+          zoom: 10
         });
       }
