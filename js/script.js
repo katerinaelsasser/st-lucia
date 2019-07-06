8d248bffa2353cb70228ab688b8a1d336a1aabd0
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-07-06 19:11:17 +0000

    adding an additional symbol for the code to work

--- js/script.js
+++ js/script.js
@@ -26,8 +26,18 @@ var map;
     	);
     
     // Custom modal 
-        var customModal = $('<div id="mapModal" class="modal map-modal" tabindex="-1" role="dialog" aria-labelledby="map-modal" aria-hidden="true"><div class="vertical-alignment-helper"><div class="modal-dialog vertical-align-center modal-sm"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h4 class="modal-title">'+ModalTitle+'</h4></div><div class="modal-body">'+alert+'<img src="'+ModalIMG+'"><p>'+ModalContent+'</p>'</div></div></div></div>');
+        var customModal = $('<div id="mapModal" class="modal map-modal" tabindex="-1" role="dialog" aria-labelledby="map-modal" aria-hidden="true"><div class="vertical-alignment-helper"><div class="modal-dialog vertical-align-center modal-sm"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h4 class="modal-title">'+ModalTitle+'</h4></div><div class="modal-body"><p>'+ModalContent+'</p></div>');
     
      $('body').append(customModal);
           $("#mapModal").modal();
+          
+          // Check if hotel, add class if true
+    var hotel = self.args.hotel;
+    //console.log(hotel);
+var pulse = [];
+if (hotel === true) {
+    var pulse = ' current-hotel';
+}
   }
+  
+  
