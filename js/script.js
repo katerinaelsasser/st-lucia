0ee058c37996b8c2a672df69136daac701787368
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-07-08 17:41:45 +0000

    adding multiple markers on the html page and adding code to create the multiple markers

--- js/script.js
+++ js/script.js
@@ -6,46 +6,24 @@ var map;
       zoom: 10
     });
     
-  //code for the marker  
-  var markers = [];
-      for(var i = 0; i < json.length; i++) {
-        // Current object
-        var obj = json[i];
-        var Modalcontent = obj.content;
-        var hotel = obj.hotel;
-        var address = obj.title;
-
-        var marker = new CustomMarker(
-    		MyLatLng,
-    		map,
-        {
-      		hotel: hotel,
-          title: address,
-          content: Modalcontent,
-    		}
-
-    	);
-
-     } 
-  
-      div = this.div = $('' +
-'<div class="spot'+ pulse +' map-location" data-toggle="tooltip">' +
-            '<div></div>' +
-            '</div>' +
-            '')[0];
+    //code to create cluster of locations
     
-    // Custom modal 
-        var customModal = $('<div id="mapModal" class="modal map-modal" tabindex="-1" role="dialog" aria-labelledby="map-modal" aria-hidden="true"><div class="vertical-alignment-helper"><div class="modal-dialog vertical-align-center modal-sm"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h4 class="modal-title">'+ModalTitle+'</h4></div><div class="modal-body"><p>'+ModalContent+'</p></div>');
-    
-     $('body').append(customModal);
-          $("#mapModal").modal();
-          
-          // Check if hotel, add class if true
-    var hotel = self.args.hotel;
-    //console.log(hotel);
-var pulse = [];
-if (hotel === true) {
-    var pulse = ' current-hotel';
-}
+   var marker = new MarkerWithLabel({
+            position: myLatlng,
+            map: map,
+            title: data.title,
+            labelContent: i,
+            labelAnchor: new google.maps.Point(7, 30),
+            labelClass: "labels", // the CSS class for the label
+            labelInBackground: false
+         });
+
+        (function (marker, data) {
+            google.maps.event.addListener(marker, "click", function (e) {
+                infoWindow.setContent(data.description);
+                infoWindow.open(map, marker);
+            });
+        })(marker, data);
+ 
   }
   
