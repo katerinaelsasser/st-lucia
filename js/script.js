57717108b78dd48c57d6a0800be4583a85526785
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-07-08 20:55:43 +0000

    relocating the } to the end of the js file

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ var map;
       center: {lat: 13.9094, lng: -60.9789},
       zoom: 10
     });
-}
     //code to create cluster of locations
     for (i = 1; i <= markers.length; i++) {
         var data = markers[i-1];
@@ -24,3 +23,5 @@ var map;
                 infoWindow.open(map, marker);
             });
         })(marker, data);
+        
+}
