528c368c74e382fffae1df7397bd6e9dd1bef143
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-07-10 18:27:47 +0000

    relocating the locations of the markers on to the js page from the html page

--- js/script.js
+++ js/script.js
@@ -17,4 +17,17 @@ var map;
         
         var markerCluster = new MarkerClusterer(map, markers,
             {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-      }
+    var locations = [
+        //royalton resort
+        {lat:14.0969, lng:-60.9498},
+        //st james club morgan bay
+        {lat:14.0406, lng:-60.9706},
+        //jade mountain resort
+        {lat:13.8646, lng:-61.0757},
+        //mango beach inn
+        {lat:13.9676, lng:-61.0244},
+        //hummingbird beach resort
+        {lat:13.8583, lng:-61.0622}
+        ]  
+    
+  }
