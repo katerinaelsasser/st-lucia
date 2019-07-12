26dea32115924743739b4963e86bb3b2fb551e52
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-07-12 19:47:50 +0000

    changing another initialise to initmap

--- js/script.js
+++ js/script.js
@@ -59,7 +59,7 @@ function initMap() {
 		zoom: 10,
 		center: myLatlng,
 		disableDefaultUI: true
-	}
+	};
 	
 	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 	
@@ -70,7 +70,7 @@ function initMap() {
 	);
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', initMap);
 
 
 
