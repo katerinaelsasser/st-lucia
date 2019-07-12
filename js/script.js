cbe8289ff786122278461508cc735e79d82e97f7
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-07-12 21:28:23 +0000

    adding more js coded to create the modal on the marker

--- js/script.js
+++ js/script.js
@@ -66,7 +66,15 @@ var map;
 		{}
 	);
 }
+//the modal for the info window
+ var customModal = $('<div id="mapModal" class="modal map-modal" tabindex="-1" role="dialog" aria-labelledby="map-modal" aria-hidden="true"><div class="vertical-alignment-helper"><div class="modal-dialog vertical-align-center modal-sm"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h4 class="modal-title">'+ModalTitle+'</h4></div><div class="modal-body">'+'<p>'+ModalContent+'</p><a class="btn'+'</a></div></div></div></div></div>');
 
+          $('body').append(customModal);
+          $("#mapModal").modal(); // lanch the modal
+
+          $('#mapModal').on('hidden.bs.modal', function (e) {
+          	$(this).remove(); // destroy the modal
+          });
 //marker
 var markers = [];
       for(var i = 0; i < json.length; i++) {
