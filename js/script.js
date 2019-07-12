c62b259cf6a0634158e538ed0c5743251b4b7b4a
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-07-12 19:43:37 +0000

    testing a new way of creating the map and adding custom markers

--- js/script.js
+++ js/script.js
@@ -1,20 +1,81 @@
-//code for the map
-var map;
-  function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 13.9094, lng: -60.9789},
-      zoom: 10
-    });
-    //code to create cluster of locations
-    var marker = new google.maps.Marker({
-    position: locations,
-    map: map,
-  });
-    
-    
-    var location = new google.maps.LatLng(14.0969, -60.9498);
-    
-    
+CustomMarker.prototype = new google.maps.OverlayView();
+
+CustomMarker.prototype.draw = function() {
+	
+	var self = this;
+	
+	var div = this.div;
+	
+	if (!div) {
+	
+		div = this.div = document.createElement('div');
+		
+		div.className = 'marker';
+		
+		div.style.position = 'absolute';
+		div.style.cursor = 'pointer';
+		div.style.width = '20px';
+		div.style.height = '20px';
+		div.style.background = 'blue';
+		
+		if (typeof(self.args.marker_id) !== 'undefined') {
+			div.dataset.marker_id = self.args.marker_id;
+		}
+		
+		google.maps.event.addDomListener(div, "click", function(event) {			
+			google.maps.event.trigger(self, "click");
+		});
+		
+		var panes = this.getPanes();
+		panes.overlayImage.appendChild(div);
+	}
+	
+	var point = this.getProjection().fromLatLngToDivPixel(this.latlng);
+	
+	if (point) {
+		div.style.left = point.x + 'px';
+		div.style.top = point.y + 'px';
+	}
+};
+
+CustomMarker.prototype.remove = function() {
+	if (this.div) {
+		this.div.parentNode.removeChild(this.div);
+		this.div = null;
+	}	
+};
+
+CustomMarker.prototype.getPosition = function() {
+	return this.latlng;	
+};
+
+//initialising the map
+
+function initialize() {
+
+	var myLatlng = new google.maps.LatLng(13.9094,-60.9789);
+	
+	var mapOptions = {
+		zoom: 10,
+		center: myLatlng,
+		disableDefaultUI: true
+	}
+	
+	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	
+	var overlay = new CustomMarker(
+		myLatlng, 
+		map,
+		{}
+	);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+
+
     /*
     var locations = [
         //royalton resort
