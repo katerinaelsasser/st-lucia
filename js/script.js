82c70033e4e8bd75905a4b14a7cf8626f11cd9f7
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-07-17 18:42:09 +0000

    trying another method to create the multiple info windows

--- js/script.js
+++ js/script.js
@@ -1,58 +1,59 @@
         //code for the marker locations
-        var markersOnMap = [{
-                placeName: "royalton resort",
-                    LatLng: [{
-                        lat: 14.0969,
-                        lng: -60.9498
-                    }]
-                },
-                {
-                    placeName: "st james club morgan bay",
-                    content: "test",
-                    LatLng: [{lat: 14.0406, lng: -60.9706}]
-                },
-                {
-                    placeName: "jade mountain resort",
-                    content: "test",
-                    LatLng: [{lat: 13.8646, lng: -61.0757}]
-                },
-                {
-                    placeName: "mango beach inn",
-                    content: "test",
-                    LatLng: [{lat: 13.9676, lng: -61.0244}]
-                },
-                {
-                    placeName: "hummingbird beach resort",
-                    content: "test",
-                    LatLng: [{lat: 13.8583, lng: -61.0622}]
-                }
+        var locations = [
+                [
+                'royalton resort',
+                '<p>TEST</p>',
+                14.0969,
+                -60.9498
+                ],
+                [
+                'st james club morgan bay',
+                '<p>TEST</p>',
+                14.0406, 
+                -60.9706
+                ],
+                [
+                'jade mountain resort',
+                '<p>TEST</p>',
+                13.8646, 
+                -61.0757
+                ],
+                [
+                'mango beach inn',
+                '<p>TEST</p>',
+                13.9676,
+                -61.0244
+                ],
+                [
+                'hummingbird beach resort',
+                '<p>TEST</p>',
+                13.8583,
+                -61.0622
+                ],
+                
         ];
  
         window.onload = function () {
             initMap();
         };
  
-        function addMarkerInfo() {
-            for (var i = 0; i < markersOnMap.length; i++) {
-                var contentString = '<div id="content"><h1>' + markersOnMap[i].placeName + '</h1>' + markersOnMap[i].content + '</p></div>';
  
-                var marker = new google.maps.Marker({
-                    position: markersOnMap[i].LatLng[0],
-                    map: map
-                });
- 
-                var infowindow = new google.maps.InfoWindow({
-                    content: contentString
-                });
-
-                marker.addListener('click', function() {
-                    infowindow.open(map, marker);
-                    closeOtherInfo();
-                        infowindow.open(marker.get('map'), marker);
-                });
-                
-            }
-        }
+    infowindow = new google.maps.InfoWindow();
+        for(i=0; i<locations.length; i++) {
+    	var position = new google.maps.LatLng(locations[i][2], locations[i][3]);
+		var marker = new google.maps.Marker({
+			position: position,
+			map: map,
+		});
+		google.maps.event.addListener(marker, 'click', (function(marker, i) {
+			return function() {
+				infowindow.setContent(locations[i][1]);
+				infowindow.setOptions({maxWidth: 200});
+				infowindow.open(map, marker);
+			}
+		}) (marker, i));
+		Markers[locations[i][4]] = marker;
+	}
  
  google.maps.event.addListener(marker, 'click', function() {
    infowindow.open(map,marker);
