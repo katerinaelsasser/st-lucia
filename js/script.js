7d7c94c7977fe20f0db2fd37dae4429c2ad88827
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-07-20 10:41:41 +0000

    going back to the js code that I have used previously to fix the issue of the missing map and markers

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
         //code for the marker locations
         var location = [
                 [
-                'title':'royalton resort',
-                'about':'<p>TEST</p>',
-                14.0969,
-                -60.9498
+                title: 'royalton resort',
+                about: '<p>TEST</p>',
+                LatLng: [{lat:14.0969, lng:-60.9498}]
                 ],
                 [
                 'title':'st james club morgan bay',
@@ -21,8 +20,7 @@
                 [
                 'title':'mango beach inn',
                 'about':'<p>TEST</p>',
-                13.9676,
-                -61.0244
+                LatLng: [{lat:13.9676, lng:-61.0244}]
                 ],
                 [
                 'title':'hummingbird beach resort',
@@ -33,33 +31,26 @@
                 
         ];
  
-        window.onload = function () {
-            initMap();
-        };
-        //creating the content of the markers
-        var Content =
-	'<h2>' + Location.title + '</h2>' +
-	'<br />' +
-	'<p>' +
-		Location.about
-	'</p>';
-     
-     //creating the object
-     infoWindow = new google.maps.InfoWindow({ content: sContent });
-        
- //creating the information for the multiple markers   
- marker = new google.maps.Marker({
-            position: position,
-            map:      map,
-            title:    markers[i][0],
-            info:     Content
-        });
- 
- //creating the function of the info window
-            google.maps.event.addListener( marker, 'click', function() {
-               infoWindow.setContent( this.info );
-               infoWindow.open( map, this );
+     infowindow = new google.maps.InfoWindow();
+
+        for(i=0; i<locations.length; i++) {
+        	var position = new google.maps.LatLng(locations[i][2], locations[i][3]);
+        	var marker = new google.maps.Marker({
+        	    position: position,
+    	        map: map,
             });
+    
+    
+     
+    		google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                return function() {
+    				infowindow.setContent(locations[i][1]);
+    				infowindow.setOptions({maxWidth: 200});
+    				infowindow.open(map, marker);
+    			}
+    		}) (marker, i));
+            Markers[locations[i][4]] = marker;
+        }
  
         
  //code for creating the map
