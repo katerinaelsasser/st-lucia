4d8f23bf738bbc5a2d52eb1430b57d89fca2f2c9
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-07-21 15:21:10 +0000

    deleting the js file with the markers on and putting the markers on the main js page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
- //code for creating the map
+var locations = [
+     //Royalton resort
+    ['Royalton resort', 14.0969 , 60.9498],
+    //St James Club Morgan Bay
+    ['St James Club Morgan Bay', 14.0406, 60.9706],
+    //Jade Mountain Resort
+    ['Jade Mountain Resort', 13.8646, 61.0757],
+  ];
+
+//code for creating the map
         function initMap() {
             map = new google.maps.Map(document.getElementById('map'), {
                 zoom: 10,
@@ -21,3 +30,6 @@
                         })(marker, count));
                       }
                     }
+                    
+  
+
