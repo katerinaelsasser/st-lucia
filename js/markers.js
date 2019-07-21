6e370467633164e5ed75fd0e2ebc661b30b7d851
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-07-21 14:05:59 +0000

    creating an additional file to hold the locations with all the info

---
+++ js/markers.js
@@ -0,0 +1,9 @@
+var locations = [
+    //Royalton resort
+    ['Royalton resort', 14.0969 , 60.9498],
+    //St James Club Morgan Bay
+    ['St James Club Morgan Bay', 14.0406, 60.9706],
+    //Jade Mountain Resort
+    ['Jade Mountain Resort', 13.8646, 61.0757],
+
+];
--- js/script.js
+++ js/script.js
@@ -5,24 +5,23 @@
                 center: {lat: 13.9094, lng: -60.9789}
             });
 
-                var infobox = new InfoBox({
-                 content: document.getElementById("infobox"),
-                 disableAutoPan: false,
-                 maxWidth: 150,
-                 pixelOffset: new google.maps.Size(-140, 0),
-                 zIndex: null,
-                 boxStyle: {
-                    background: "url('http://google-maps-utility-library-v3.googlecode.com/svn/trunk/infobox/examples/tipbox.gif') no-repeat",
-                    opacity: 0.90,
-                    width: "300px"
-                },
-                closeBoxMargin: "12px 4px 2px 2px",
-                closeBoxURL: "http://www.google.com/intl/en_us/mapfiles/close.gif",
-                infoBoxClearance: new google.maps.Size(1, 1)
-            });
-            
-            google.maps.event.addListener(marker, 'click', function() {
-                infobox.open(map, this);
-                map.panTo(lat_lng);
-            });
-        }
+                for (var i = 0; i < data.length; i++) {
+      var current = data[i];
+  
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(current.position.lat, current.position.lng),
+        map: map,
+        content: current.content
+      });
+  
+      markers.push(marker);
+  
+      google.maps.event.addListener(markers[i], "click", function (e) {
+        var infoBox = new InfoBox({
+            latlng: this.getPosition(),
+            map: map,
+            content: this.content
+        });
+      });
+    }
+}
