a3bf94173256b8c641c87ad728f340a923a52d48
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-07-22 18:57:08 +0000

    adding more information to other info windows on the js file

--- js/markers.js
+++ js/markers.js
@@ -1,11 +1,11 @@
 var locations = [
      //Hotels
-    ['<h2>Royalton Resort</h2><p>Hotel</p><p>Treat yourself to this luxuarious resort and spa destination on the carribean island. This resort is split into 3 sections: the resort, the Diamond Club and the (adults only) Hideaway.</p>', 14.0969 , -60.9498],
-    ['<h2>St James Club Morgan Bay</h2><p>Hotel</p>', 14.0406, -60.9706],
-    ['<h2>Jade Mountain Resort</h2><p>Hotel</p>', 13.8646, -61.0757],
-    ['<h2>Mango Beach Inn</h2><p>Hotel</p>', 13.9676, -61.0244],
-    ['<h2>Hummingbird Beach Resort</h2><p>Hotel</p>', 13.8583, -61.0622],
-    ['<h2>Marigot Beach Club & Dive Resort</h2><p>Hotel</p>', 13.9667, -61.0257],
+    ['<h2>Royalton Resort & Spa</h2><p>Hotel</p><p>Treat yourself to this luxuarious all-inclusive resort destination which over looks the carribean sea. This resort is split into 3 sections: the resort, the Diamond Club and the (adults only) Hideaway.<li>Suitable for couples and families.</li></p>', 14.0969 , -60.9498],
+    ['<h2>St James Club Morgan Bay</h2><p>Hotel</p><p>This relaxing location for everyone. This is an all inclusive which includes meals and beaerages.<li>Suitable for families and couples.</p>', 14.0406, -60.9706],
+    ['<h2>Jade Mountain Resort</h2><p>Hotel</p><p>This Resort is the only property with the iconic view of both the Piti and Gros Piton mountains.<li>Suitable for couples</li></p>', 13.8646, -61.0757],
+    ['<h2>Mango Beach Inn</h2><p>Hotel</p><p>This location is ideal if you are curious about history, culture and music as well as rainforest, waterfalls, beaches and nature trails.<li>Suitable for families and couples</li></p>', 13.9676, -61.0244],
+    ['<h2>Hummingbird Beach Resort</h2><p>Hotel</p><p>This affordable resort is perfect for nature lovers.<li>Suitable for couples and families</li></p>', 13.8583, -61.0622],
+    ['<h2>Marigot Beach Club & Dive Resort</h2><p>Hotel</p><p>Travel to this stunning and secluded location.<li>Suitable for families and couples.</p>', 13.9667, -61.0257],
     ['<h2>Coco Palm Resort</h2><p>Hotel</p>', 14.0717, -60.9566],
     ['<h2>Marigot Bay Resort and Marina</h2><p>Hotel</p>', 13.9644, -61.0230],
     ['<h2>Samfi Gardens</h2><p>Hotel</p>', 13.8661, -61.0517],
