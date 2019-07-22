acdd8323fe3ab11b1e9d86442aa3c2283fe58403
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-07-22 17:20:04 +0000

    adding a new js file to hold all of the markers and their info, and linking it to the html page

---
+++ js/markers.js
@@ -0,0 +1,28 @@
+var locations = [
+     //Hotels
+    ['<h2>Royalton Resort</h2><p>Hotel</p>', 14.0969 , -60.9498],
+    ['<h2>St James Club Morgan Bay</h2><p>Hotel</p>', 14.0406, -60.9706],
+    ['<h2>Jade Mountain Resort</h2><p>Hotel</p>', 13.8646, -61.0757],
+    ['<h2>Mango Beach Inn</h2><p>Hotel</p>', 13.9676, -61.0244],
+    ['<h2>Hummingbird Beach Resort</h2><p>Hotel</p>', 13.8583, -61.0622],
+    ['<h2>Marigot Beach Club & Dive Resort</h2><p>Hotel</p>', 13.9667, -61.0257],
+    ['<h2>Coco Palm Resort</h2><p>Hotel</p>', 14.0717, -60.9566],
+    ['<h2>Marigot Bay Resort and Marina</h2><p>Hotel</p>', 13.9644, -61.0230],
+    ['<h2>Samfi Gardens</h2><p>Hotel</p>', 13.8661, -61.0517],
+    ['<h2>Têt Rouge Resort</h2><p>Hotel</p>', 13.7931, -61.0625],
+    ['<h2>Starfish St. Lucia</h2><p>Hotel</p>', 14.0717, -60.9566],
+    ['<h2>The Harbor Club</h2><p>Hotel</p>', 14.0731, -60.9476],
+    ['<h2>Ladera Resort</h2><p>Hotel</p>', 13.8302, -61.0515],
+    ['<h2>Ladera Resort</h2><p>Hotel</p>', 14.0589, -60.9742],
+    
+    //Resturant
+    //The Cluff At Cap
+    //Big Chef Steakhouse
+    //Rainforest Hideaway
+    //Dasheene
+    //Boucan
+    //The Coal Pit
+    //Fox Grove Inn
+    //Flavours of the Grill
+    //Cafe Ole
+  ];
