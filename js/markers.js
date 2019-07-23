22b97e88ba947d7cee148499295a1906685b9f92
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-07-23 20:50:12 +0000

    adding more information to the markers on the js file

--- js/markers.js
+++ js/markers.js
@@ -9,12 +9,11 @@ var locations = [
     ['<h2>Coco Palm Resort</h2><p>Hotel</p><p>This award winning bouquet hotel is the perfect destination if you like classic Carribean French Creole decor.<li>Suitable for families and couples.</li></p>', 14.0717, -60.9566],
     ['<h2>Marigot Bay Resort and Marina</h2><p>Hotel</p><p>At this St Lucian resort, you will experience the culture, cuisine and the traditions.<li>Suitable for couples and families.</li></p>', 13.9644, -61.0230],
     ['<h2>Samfi Gardens</h2><p>Hotel</p><p>This is a newly established family owned property. This has 8 self-contained villas; 7 of them are called Vacation homes and 1 is called Honeymoon Suite.<li>Suitable for families and couples.</li></p>', 13.8661, -61.0517],
-    ['<h2>Têt Rouge Resort</h2><p>Hotel</p>', 13.7931, -61.0625],
-    ['<h2>Starfish St. Lucia</h2><p>Hotel</p>', 14.0717, -60.9566],
-    ['<h2>The Harbor Club</h2><p>Hotel</p>', 14.0731, -60.9476],
-    ['<h2>Ladera Resort</h2><p>Hotel</p>', 13.8302, -61.0515],
-    ['<h2>Ladera Resort</h2><p>Hotel</p>', 14.0589, -60.9742],
-    
+    ['<h2>Têt Rouge Resort</h2><p>Hotel</p><p>This villa is the perfect getaway for all. With 6 sea-view studios featuring a specious terrace looking on to the blue Carribean. This is located near the Gros Piton.<li>Suitable for couples.</li></p>', 13.7931, -61.0625],
+    ['<h2>Starfish St. Lucia</h2><p>Hotel</p><p>This tropical escape is ideal if you love white sand beaches and crystal clear water.<li>Suitable for families and couples.</li></p>', 14.0717, -60.9566],
+    ['<h2>The Harbor Club</h2><p>Hotel</p><p>Part of the Hilton collection. Experience the tropical beauty and the maritime charm.<li>Suitable for families and couples.</li></p>', 14.0731, -60.9476],
+    ['<h2>Ladera Resort</h2><p>Hotel</p><p>Experience St Lucia with this resprt whichs is set at the edge of the rainforest, overlooking both the Carribesn sea and the Piton Mountains.<li>Suitable for couples.</li></p>', 13.8302, -61.0515],
+
     //Resturant
     //The Cluff At Cap
     //Big Chef Steakhouse
