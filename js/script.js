118e5c3d69da7a146400de06a448ab8a77dfa9b4
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-08-05 17:47:02 +0000

    addingjs code that will create the features on the info window

--- js/script.js
+++ js/script.js
@@ -35,7 +35,8 @@ var icons = {
                     var marker, count;
                     for (count = 0; count < locations.length; count++) {
                         marker = new google.maps.Marker({
-                          position: new google.maps.LatLng(locations[count][1], locations[count][2]),
+                          position: locations.position,
+                          content: locations.content,
                           map: map,
                           icon: icons[locations.type].icon,
                         });
