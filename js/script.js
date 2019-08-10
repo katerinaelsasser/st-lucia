867c66910f6060035c65ce52df6b50ac36df8c09
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-08-10 19:34:12 +0000

    adding more code to the icon section on the script page to fix the issue with the link to the images

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ var icon = {
                           position: new google.maps.LatLng(locations[count][1], locations[count][2]),
                           map: map,
                           title: locations[count][0],
-                          icon: locations[count][3],
+                          icon: icon.locations[count][3].url,
                           
                         });
                     google.maps.event.addListener(marker, 'click', (function (marker, count) {
