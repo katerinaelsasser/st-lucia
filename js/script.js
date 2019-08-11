5d6a7a22926958e3b1f7f63c170a707ab4106760
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-08-11 12:18:23 +0000

    adding the icon code again to see if i can fix the issue, using the example from google

--- js/script.js
+++ js/script.js
@@ -36,6 +36,7 @@ var icons = {
                         marker = new google.maps.Marker({
                           position: new google.maps.LatLng(locations[count][1], locations[count][2]),
                           title: locations[count][0],
+                          icon: icons[location.type].icon,
                           map: map,
                         });
                     google.maps.event.addListener(marker, 'click', (function (marker, count) {
