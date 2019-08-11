a08deace88ec2e385c1aff4275be0eb604f9e053
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-08-11 13:10:00 +0000

    addding sizing to the icons on the main page and removing the [count] code from the marker section as i believe it is not needed

--- js/script.js
+++ js/script.js
@@ -15,16 +15,28 @@
 var icons = {
           hotel: {
             icon: 'assets/images/markers/lodging.png',
+            size: new google.maps.Size(20, 32),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(0, 32)
           },
           attraction: {
             icon: 'assets/images/markers/attraction.png',
+            size: new google.maps.Size(20, 32),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(0, 32)
               
           },
           restaurant: {
             icon: 'assets/images/markers/restaurant.png',
+            size: new google.maps.Size(20, 32),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(0, 32)
           },
           airport: {
             icon: 'https://mapicons.mapsmarker.com/wp-content/uploads/mapicons/shape-default/color-b05ecc/shapecolor-color/shadow-1/border-dark/symbolstyle-white/symbolshadowstyle-dark/gradient-no/airport.png',
+            size: new google.maps.Size(20, 32),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(0, 32)
           }
         };
         
@@ -34,8 +46,8 @@ var icons = {
                     var marker, count;
                     for (count = 0; count < locations.length; count++) {
                         marker = new google.maps.Marker({
-                          position: new google.maps.LatLng(locations[count][1], locations[count][2]),
-                          title: locations[count][0],
+                          position: new google.maps.LatLng(locations[1], locations[2]),
+                          title: locations[0],
                           icon: icons[locations.type].icon,
                           map: map,
                         });
