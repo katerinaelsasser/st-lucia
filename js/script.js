d5767ac6650c3bcfc26f2b6c9e45fb250cd02b11
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-08-19 18:12:54 +0000

    changing the code for the marker as I have adjusted the marker data

--- js/script.js
+++ js/script.js
@@ -45,8 +45,8 @@ var icons = {
          var marker, count;
        for (count = 0; count < locations.length; count++) {
       marker = new google.maps.Marker({
-         position: new google.maps.LatLng(locations[count][2], locations[count][3]),
-       title: locations[count][0],
+         position: new google.maps.LatLng(locations[count][1], locations[count][2]),
+       content: locations[count][0],
       map: map,
       });
                     google.maps.event.addListener(marker, 'click', (function (marker, count) {
