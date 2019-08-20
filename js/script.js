8f10a292c7536da0facb5db605ed230235995125
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-08-20 19:54:20 +0000

    removing the js code for linking the icon images

--- js/script.js
+++ js/script.js
@@ -10,36 +10,6 @@
             });
 
 
-//code for the custom markers
-
-var icons = {
-          hotel: {
-            icon: 'assets/images/markers/lodging.png',
-            size: new google.maps.Size(20, 32),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(0, 32)
-          },
-          attraction: {
-            icon: 'assets/images/markers/attraction.png',
-            size: new google.maps.Size(20, 32),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(0, 32)
-              
-          },
-          restaurant: {
-            icon: 'assets/images/markers/restaurant.png',
-            size: new google.maps.Size(20, 32),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(0, 32)
-          },
-          airport: {
-            icon: 'https://mapicons.mapsmarker.com/wp-content/uploads/mapicons/shape-default/color-b05ecc/shapecolor-color/shadow-1/border-dark/symbolstyle-white/symbolshadowstyle-dark/gradient-no/airport.png',
-            size: new google.maps.Size(20, 32),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(0, 32)
-          }
-        };
-        
     //info window code
       var infowindow =  new google.maps.InfoWindow({});
        var marker, count;
