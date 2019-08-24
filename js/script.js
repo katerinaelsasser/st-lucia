27e91f6e11617631a9d6d413d4c44da2be124ae2
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-08-24 15:47:50 +0000

    adding content section on the marker scriptjs code

--- js/script.js
+++ js/script.js
@@ -15,8 +15,9 @@
       var marker, count;
        for (count = 0; count < locations.length; count++) {
       marker = new google.maps.Marker({
-         position: new google.maps.LatLng(locations[count][1], locations[count][2]),
+         position: new google.maps.LatLng(locations[count][2], locations[count][3]),
       title: locations[count][0],
+      content: locations[count][1],
     map: map,
     });
   
