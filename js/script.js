a0220e7bc6fd03d44c10ca24feab82b07ffafce9
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-08-24 11:58:52 +0000

    test failed, going back to the old code as that was correct and removing the comment code that was hiding the two airport markers

--- js/script.js
+++ js/script.js
@@ -11,35 +11,15 @@
 
 
     //info window code
-    //  var infowindow =  new google.maps.InfoWindow({});
-     //  var marker, count;
-      // for (count = 0; count < locations.length; count++) {
-      //marker = new google.maps.Marker({
-    //     position: new google.maps.LatLng(locations[count][1], locations[count][2]),
-     //  title: locations[count][0],
-      //map: map,
-      //});
-      
-      $("#link1").click(function(){
-    changeMarkerPos(14.0200, -60.9931);
-});
-$("#link2").click(function(){
-    changeMarkerPos(13.7334, -60.9503);
-});
-
-marker.setMap(map);
-    map.panTo(marker.position);
-}
-
-function changeMarkerPos(lat, lon){
-    myLatLng = new google.maps.LatLng(lat, lon)
-    marker.setPosition(myLatLng);
-    map.panTo(myLatLng);
-}
-      
-      
-      
-      
+    var infowindow =  new google.maps.InfoWindow({});
+      var marker, count;
+       for (count = 0; count < locations.length; count++) {
+      marker = new google.maps.Marker({
+         position: new google.maps.LatLng(locations[count][1], locations[count][2]),
+      title: locations[count][0],
+    map: map,
+    });
+  
       
                   google.maps.event.addListener(marker, 'click', (function (marker, count) {
                       return function () {
