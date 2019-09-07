0fd9e3348c127c9a6c3c457fed08a7fe2a5e23ea
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-09-07 17:37:25 +0000

    changing the name of the location photos

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ var infowindow =  new google.maps.InfoWindow({});
 var marker, count;
 for (count = 0; count < locations.length; count++) {
 marker = new google.maps.Marker({
-position: new google.maps.LatLng(locations.lat, locations.lng),
+ position: new google.maps.LatLng(locations.lat, locations.lng),
 title: locations.name,
 content: locations.description,
 map: map,
@@ -19,10 +19,10 @@ map: map,
   
       
                   google.maps.event.addListener(marker, 'click', (function (marker, count) {
-                      return function () {
-                      infowindow.setContent(locations.description);
-                            infowindow.open(map, marker);
-                        };
-                        })(marker, count));
-                    }
+              return function () {
+              infowindow.setContent(locations.description);
+                    infowindow.open(map, marker);
+                };
+                })(marker, count));
+            }
 }
