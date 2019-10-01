a26b6f9f48d7bbabc48616c735d19371ab64cd1b
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-10-01 20:05:01 +0000

    adding example ajax code that will add the marker

--- script.js
+++ script.js
@@ -5,9 +5,7 @@
                 center: {lat: 13.9094, lng: -60.9789},
             });
 
-
 var marker = [];
-      // Looping through all the entries from the JSON data
       for(var i = 0; i < location.length; i++) {
         
         var content = location.description;
@@ -17,6 +15,55 @@ var marker = [];
 
       };
 
+                 $.ajax({
+                   type: "GET",
+                    url: 'https://github.com/katerinaelsasser/st-lucia-destination/blob/master/data.json',
+                    dataType: "json",
+                    success: function (data) {
+                        console.log(data);
+
+                        if (data.length !== 0) {
+
+                         var   latLng = new google.maps.LatLng(data.lat, data.lng);
+
+                        // Creating a marker and putting it on the map
+                        var marker = new google.maps.Marker({
+                            position: latLng,
+                            map: map,
+                            title: data.title
+                        });
+
+
+                            $.each(data, function (marker, data) {
+                                var windowContent = '<h3>' + data.title + '</h3>' +
+                                    '<p>' + data.description + '</p>';
+                                console.log(windowContent);
+
+                                // Attaching a click event to the current marker
+                                infobox = new InfoBox({
+                                    content: infoWindow.setContent(windowContent),
+                                });
+
+                                google.maps.event.addListener(marker, 'click', function () {
+
+                                    // Open this map's infobox
+                                    infobox.open(map, marker);
+                                    infobox.setContent(windowContent);
+                                    map.panTo(marker.getPosition());
+                                    infobox.show();
+                                });
+                                google.maps.event.addListener(map, 'click', function () {
+                                    infobox.setMap(null);
+                                });
+                            });
+
+                        } 
+
+
+                    },
+
+                });
+                
 
 
 
