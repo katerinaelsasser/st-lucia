f2e138f4e111c86766749745f5cb655bf454d4cd
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-10-08 17:49:24 +0000

    adding google developer example on the js file and linking the json file

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 //code for creating the map
-        function initMap() {
-            map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 10,
-                center: {lat: 13.9094, lng: -60.9789},
-            });
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: {lat: 13.9094, lng: -60.9789},
+  });
+
+
+  var infowindow = new google.maps.InfoWindow({
+      content: data.description
+    }); 
+    
+    var icons = {
+          airport: {
+            icon: '.png'
+          },
+          attraction: {
+            icon: '.png'
+          },
+          info: {
+            icon: 's.png'
+          }
+        };
+    
+    var latlng = new google.maps.LatLng (data.lat, data.lng)
+    
+    var marker = new google.maps.Marker({
+    position: latlng,
+    map: map,
+    title: data.name,
+    icon: icons[data.type].icon,
+  });
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+  
 }
