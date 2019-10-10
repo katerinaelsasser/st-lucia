69c72fe44aec7a7336e212751c22cd2c4cdfad6e
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-10-10 19:12:50 +0000

    changing the code for the markers

--- script.js
+++ script.js
@@ -5,25 +5,6 @@ function initMap() {
     center: {lat: 13.9094, lng: -60.9789},
   });
   
-  
-  var database = new google.maps.database();
-
-var contentString = database.content;
-
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
-
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
-    title: 'Uluru (Ayers Rock)'
-  });
-  marker.addListener('click', function() {
-    infowindow.open(map, marker);
-  });
- 
-    
     var icons = {
           airport: {
             url: 'https://github.com/katerinaelsasser/st-lucia-destination/blob/master/assets/images/markers/airport.png'
@@ -39,26 +20,27 @@ var contentString = database.content;
           }
         };
     
-    //var markers = [];
+    var markers = [];
       // Looping through all the entries from the JSON data
-    //  for(var i = 0; i < data.length; i++) {
+      for(var i = 0; i < json.length; i++) {
         // Current object
-      //  var Modalcontent = data.description;
-      //  var address = data.title;
-      //  var MyLatLng = new google.maps.LatLng(data.lat,data.lng);
+        var obj = database[i];
+        var Modalcontent = obj.content;
+        var address = obj.name;
+        var icon = obj.icon;
+        var MyLatLng = new google.maps.LatLng(obj.lat,obj.lng);
 
-      //  var marker = new CustomMarker(
-    	//	MyLatLng,
-    	//	map,
-      //  {
-       //   title: address,
-        //  content: Modalcontent,
-    		//}
-
-    //	);
-marker.addListener('click', function() {
+        var marker = new CustomMarker(
+    		MyLatLng,
+    		map,
+        {
+          title: address,
+          content: Modalcontent,
+          icon: icon
+    		}
+);
+  marker.addListener('click', function() {
     infowindow.open(map, marker);
   });
+      }
 }
-  
-  
