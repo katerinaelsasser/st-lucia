6ea2777d416ebddac1b696ba0b29ed39298f0fba
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-10-21 19:23:14 +0000

    removing and hidding the icon codes and adjusting the info window coded

--- script.js
+++ script.js
@@ -5,12 +5,6 @@ function initMap() {
     center: {lat: 13.9094, lng: -60.9789},
   });
   
-  var icon = {
-    airport: 'https://github.com/katerinaelsasser/st-lucia-destination/blob/master/assets/images/markers/airport.png',
-    attraction: 'https://github.com/katerinaelsasser/st-lucia-destination/blob/master/assets/images/markers/attraction.png',
-    hotel: 'https://github.com/katerinaelsasser/st-lucia-destination/blob/master/assets/images/markers/hotel.png',
-    restaurant: 'https://github.com/katerinaelsasser/st-lucia-destination/blob/master/assets/images/markers/restaurant.png'
-  }
   
    // Looping through all the entries from the JSON data
   for(var i = 0; i < data.length; i++) {
@@ -22,7 +16,7 @@ function initMap() {
     var marker = new google.maps.Marker({
       position: new google.maps.LatLng(obj.lat,obj.lng),
       map: map,
-      icon: obj.icon,
+      //icon: obj.icon,
       title: obj.description
     });
     
@@ -32,16 +26,13 @@ function initMap() {
     
     // Adding a new info window for the object
     var clicker = addClicker(marker, obj.name);
-    
-
-  
-  
+ 
   // Adding a new click event listener for the object
   function addClicker(marker, content) {
     google.maps.event.addListener(marker, 'click', function() {
       
       if (infowindow) {infowindow.close();}
-      infowindow = new google.maps.InfoWindow({content: content});
+      infowindow = new google.maps.InfoWindow({content: obj.description});
       infowindow.open(map, marker);
     
     });
