7e4c82e9d3bf9465dd87ac64ef45ace4d6c3a58d
Ubuntu <ubuntu@ip-172-31-42-69.ec2.internal>
2019-11-07 19:27:28 +0000

    adding scroll to function on the buttons to go to the map

--- assets/js/script.js
+++ assets/js/script.js
@@ -129,4 +129,26 @@ $.getJSON("assets/data/data.json", function(json1) {
       
 }
 
-//what the info window should have in it = ('<h2>'+obj.title+'</h2><h4><i>'+obj.type+'</i></h4><p>'+obj.description+'</p>')
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
